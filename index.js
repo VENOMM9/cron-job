e48656951caefdf39191a91327bf1e593bfbd084
Victor Nkwuo <vobinn3@gmail.com>
2024-03-14 03:56:04 +0300

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const connectDB = require('./db/db')
 const birthdata = require('./model/form')
 require('dotenv').config()
 
-const port = process.env.PORT||3000
+const port = process.env.PORT||3200
 
 const app = express()
 
